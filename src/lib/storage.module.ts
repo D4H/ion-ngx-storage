74c69a9e68b2d6155a2b10e1137b2fbfc504644e
Mark <mark@bhalash.com>
2019-07-09 19:29:22 +0100

    Added DI-compatible store module.
    See: https://ngrx.io/guide/store/recipes/injecting
    See: https://stackoverflow.com/a/43246735/1433400
    See: https://ionicframework.com/docs/building/storage
    See: https://github.com/ionic-team/ionic-storage/blob/2ea8583d774a96c3c150e944c8fe925d0cf69f3d/src/index.ts
    
    Per all the above links:
    
    1. NgRx feature states-including meta reducers-are compatible with
    Angular DI, if so configured.
    2. Ionic's Storage solution is compatible with DI, though the docs
    neglect to mention this.
    
    The module sets up DI of the passed configuration, from which it
    initializes the meta reducer and storage class.

--- src/lib/storage.module.ts
+++ src/lib/storage.module.ts
@@ -1,10 +1,47 @@
-import { NgModule } from '@angular/core';
-import { StorageComponent } from './storage.component';
+import { EffectsModule } from '@ngrx/effects';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { Storage, StorageConfig, StorageConfigToken } from '@ionic/storage';
+import { ActionReducerMap, MetaReducer, StoreModule, META_REDUCERS } from '@ngrx/store';
+
+import {
+  STORAGE_CONFIG,
+  StorageModuleConfig,
+  defaultConfig,
+  provideStorage
+} from './providers';
+
+import {
+  StorageEffects,
+  storageReducerProvider,
+  reducer
+} from './store';
 
 @NgModule({
-  declarations: [StorageComponent],
   imports: [
-  ],
-  exports: [StorageComponent]
+    StoreModule.forFeature('ion-ngx-storage', reducer),
+    EffectsModule.forFeature([StorageEffects])
+  ]
 })
-export class StorageModule { }
+export class StorageModule {
+  static forFeature(configuration: StorageModuleConfig = defaultConfig): ModuleWithProviders {
+    return {
+      ngModule: StorageModule,
+      providers: [
+        {
+          provide: STORAGE_CONFIG,
+          useValue: configuration
+        },
+        {
+          deps: [STORAGE_CONFIG],
+          provide: Storage,
+          useFactory: provideStorage
+        },
+        {
+          deps: [STORAGE_CONFIG, Storage],
+          provide: META_REDUCERS,
+          useFactory: storageReducerProvider
+        }
+      ]
+    };
+  }
+}
