9d0be703f615910ecc45db6bda9c5f294984042b
Mark Grealish <mark@bhalash.com>
2019-07-16 11:22:56 +0100

    Setup tests.
    1. Added in-use Karma configuration and plugins.
    2. Resolved security vulnerability and NPM warnings.
    3. Added simple test to validate suite working.

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,49 @@
+const path = require('path');
+
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = config => {
   config.set({
+    autoWatch: true,
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    colors: true,
+    logLevel: config.LOG_INFO,
+    port: 9876,
+    restartOnFileChange: true,
+    singleRun: false,
+    browsers: [
+      'ChromeHeadless'
+    ],
+    reporters: [
+      'kjhtml',
+      'mocha',
+      'progress'
+    ],
+    frameworks: [
+      '@angular-devkit/build-angular',
+      'jasmine',
+      'jasmine-matchers'
+    ],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      '@angular-devkit/build-angular/plugins/karma',
+      'karma-chrome-launcher',
+      'karma-coverage-istanbul-reporter',
+      'karma-jasmine',
+      'karma-jasmine-html-reporter',
+      'karma-jasmine-matchers',
+      'karma-mocha-reporter'
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/storage'),
-      reports: ['html', 'lcovonly'],
+      dir: path.join(__dirname, '../../coverage/storage'),
+      reports: [
+        'html',
+        'lcovonly'
+      ],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
   });
 };
